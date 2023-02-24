b85fda0eefa4f0649badf83e70c758a2b3ed0822
William <wsuarez86@hotmail.com>
2023-02-23 22:31:35 -0500

    Sgundo commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,47 @@
+import React from 'react'
+import Button from './components/Button'
+import MathOperations from './components/MathOperations'
+import Result from './components/Result'
+import './App.css'
 
-function App() {
-    return ( 
-        <h1> Ejemplo </h1>
-    );
+// Función Flecha o Arrow Function
+const App = () => {
+
+    const clickHandlerFunction = text => {
+        console.log("Button.clickHandler1", text)
+    }
+
+    // Lo que ejecuta la función
+    console.log("Renderización de App")
+    return (
+    <main className='react-calculator'>
+        <Result value={undefined} />
+        <div className="numbers">
+            <Button text="1" clickHandler={clickHandlerFunction} /> 
+            <button>2</button>
+            <button>3</button>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button>0</button>
+        </div>
+        <div className="functions">
+            <button>
+                clear
+            </button>
+            <button>
+                r
+            </button>
+        </div>
+        <MathOperations 
+           onClickOperation={(operation) => console.log("Operations:", operation)} 
+        />
+    </main>
+    )
 }
 
-export default App;
+export default App
+
