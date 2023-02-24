4613e6016f8e58b4d2484fedcd0f144f38c46001
William Suáres <william.suares@ucacue.edu.ec>
2023-02-24 10:21:59 -0500

    Se agrega funcionalidades.

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,34 @@
-import React from "react";
+/* eslint no-eval: 0 */
+import React, { useState } from "react";
 import MathOperations from "./components/MathOperations";
 import Result from "./components/Result";
 import Functions from "./components/Functions";
 import Numbers from "./components/Numbers";
+import words from "lodash.words";
 import "./App.css";
 
 const App = () => {
+
+    const [statck, setStatck] = useState("");
+    const items = words(statck, /[^-^+^*^/]+/g);
+    const value = items.length > 0 ? items[items.length-1] : "0";
+
   return (
     <main className="react-calculator">
-      <Result value={undefined} />
-      <Numbers onClickNumber={(number) => console.log("Number:", number)} />
+      <Result value={value} />
+      <Numbers onClickNumber={(number) => setStatck(statck + number)} />
       <Functions
-        onContentClear={() => console.log("Clear")}
-        onDelete={() => console.log("onDelete")}
+        onContentClear={() => setStatck("")}
+        onDelete={() => {
+            if(statck.length > 0){
+                const newStatck = statck.substring(0, statck.length - 1);
+                setStatck(newStatck);
+            }
+        }}
       />
       <MathOperations
-        onClickOperation={(operation) => console.log("Operations:", operation)}
-        onClickEqual={(equal) => console.log("Igual:", equal)}
+        onClickOperation={(operation) => setStatck(statck + operation)}
+        onClickEqual={(equal) => setStatck(eval(statck).toString())}
       />
     </main>
   );
